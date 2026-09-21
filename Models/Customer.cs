88d0bccdd846ca39a2bac0c620faea52ca9486f1
Michael Gustavsson <michael.gustavsson@softtech-dev.se>
2026-09-21 13:38:48 +0200

    First naive design

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -2,5 +2,17 @@
 
 public class Customer
 {
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string FirstName { get; set; } = "";
+    public string LastName { get; set; } = "";
+    public string Email { get; set; } = "";
+    public string Phone { get; set; } = "";
+    public string AddressLine { get; set; } = "";
+    public string PostalCode { get; set; } = "";
+    public string City { get; set; } = "";
+    public string Country { get; set; } = "";
 
+    public void AddCustomer() { }
+    public void UpdateCustomer() { }
+    public void RemoveCustomer() { }
 }
--- Models/Vehicle.cs
+++ Models/Vehicle.cs
@@ -2,5 +2,28 @@
 
 public class Vehicle
 {
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string RegistrationNumber { get; set; } = "";
+    public string Manufacturer { get; set; } = "";
+    public string Model { get; set; } = "";
+    public int ModelYear { get; set; }
+    public int Mileage { get; set; }
+    public string Color { get; set; } = "";
+    public string Description { get; set; } = "";
+    public double Price { get; set; }
+    public bool IsSold { get; set; }
+    public string GearType { get; set; } = "";
+    public string TransmissionType { get; set; } = "";
+    public int HorsePower { get; set; }
+    public int EngineEffect { get; set; }
+    public double EngineVolume { get; set; }
+    public string BikeType { get; set; } = "";
+    public bool TailLift { get; set; }
+    public int MaxLoad { get; set; }
+    public int Tara { get; set; }
+    public List<string> Equipments { get; set; } = [];
 
+    public void AddVehicle() { }
+    public void UpdateVehicle() { }
+    public void RemoveVehicle() { }
 }
